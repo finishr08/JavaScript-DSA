f1fc4255ecbaa8ad679a892a854152b9dd96d935
Mustafa Ahmed <contactmustafa8@gmail.com>
2025-05-31 14:13:47 +0500

    Implement advanced array rotation (left/right) using two-pointer reversal approach

--- 2. JavaScript Advance/4. Advance Array/leftRotationByK.js
+++ 2. JavaScript Advance/4. Advance Array/leftRotationByK.js
@@ -1,17 +1,18 @@
-// let arr = [1, 2, 3, 4, 5];
-// let k = Number(prompt("Enter the value of k: "));
+/** 
+let arr = [1, 2, 3, 4, 5];
+let k = Number(prompt("Enter the value of k: "));
 
-// k = k % arr.length;
+k = k % arr.length;
 
-// for (let j = 0; j < k; j++) {
-//   let last = arr[arr.length - 1];
-//   for (let i = arr.length - 1; i > 0; i--) {
-//     arr[i] = arr[i - 1];
-//   }
-//   arr[0] = last;
-// }
+for (let j = 0; j < k; j++) {
+  let last = arr[arr.length - 1];
+  for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = last;
+}
 
-// console.log(arr);
+console.log(arr);
 
 // Better Approach
 
@@ -25,3 +26,27 @@ for (let i = 0; i < arr.length; i++) {
   tempArray[i] = arr[(i - k + arr.length) % arr.length];
 }
 console.log(tempArray);
+
+*/
+
+// Two Pointer Technique
+
+let arr = [1, 2, 3, 4, 5];
+
+let k = Number(prompt("Enter the value of k: "));
+k = k % arr.length;
+
+function Reverse(i, j) {
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  }
+}
+
+Reverse(0, k - 1);
+Reverse(k, arr.length - 1);
+Reverse(0, arr.length - 1);
+console.log(arr);
--- 2. JavaScript Advance/4. Advance Array/rightRotationByK.js
+++ 2. JavaScript Advance/4. Advance Array/rightRotationByK.js
@@ -1,17 +1,18 @@
-// let arr = [1, 2, 3, 4, 5];
-// let k = Number(prompt("Enter the value of k: "));
+/** 
+let arr = [1, 2, 3, 4, 5];
+let k = Number(prompt("Enter the value of k: "));
 
-// k = k % arr.length;
+k = k % arr.length;
 
-// for (let j = 0; j < k; j++) {
-//   let first = arr[0];
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     arr[i] = arr[i + 1];
-//   }
-//   arr[arr.length - 1] = first;
-// }
+for (let j = 0; j < k; j++) {
+  let first = arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[arr.length - 1] = first;
+}
 
-// console.log(arr);
+console.log(arr);
 
 // Better Approach
 
@@ -25,3 +26,28 @@ for (let i = 0; i < arr.length; i++) {
   tempArray[i] = arr[(i + k) % arr.length];
 }
 console.log(tempArray);
+
+*/
+
+// Two Pointer Technique
+
+let arr = [1, 2, 3, 4, 5];
+
+let k = Number(prompt("Enter the value of k: "));
+k = k % arr.length;
+
+function Reverse(i, j) {
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  }
+}
+
+Reverse(0, arr.length - 1);
+Reverse(0, arr.length - k - 1);
+Reverse(arr.length - k, arr.length - 1);
+
+console.log(arr);
