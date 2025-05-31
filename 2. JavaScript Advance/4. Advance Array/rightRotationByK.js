c1fe6b834745614fc977be0962590b05e31d113d
Mustafa Ahmed <contactmustafa8@gmail.com>
2025-05-31 13:55:50 +0500

    Advance Array (left,Right Rotation of Elements { Better Approach })

--- 2. JavaScript Advance/4. Advance Array/rightRotationByK.js
+++ 2. JavaScript Advance/4. Advance Array/rightRotationByK.js
@@ -1,3 +1,20 @@
+// let arr = [1, 2, 3, 4, 5];
+// let k = Number(prompt("Enter the value of k: "));
+
+// k = k % arr.length;
+
+// for (let j = 0; j < k; j++) {
+//   let first = arr[0];
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     arr[i] = arr[i + 1];
+//   }
+//   arr[arr.length - 1] = first;
+// }
+
+// console.log(arr);
+
+// Better Approach
+
 let arr = [1, 2, 3, 4, 5];
 let tempArray = new Array(arr.length);
 let k = Number(prompt("Enter the value of k: "));
